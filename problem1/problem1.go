243ec39a34ab843c315b3bd72304d0ff4343a571
ardhiseptianalqohar <ardhiseptianalqohar@gmail.com>
2022-08-03 18:53:23 +0700

    problem1

---
+++ problem1/problem1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// input name = "kobar"
+	// output = "Hello kobar!, Saya Golang bahasa yang sangat menyenangkan"
+
+	var nama string = "Kobar"
+	fmt.Println(nama)
+
+
+}
