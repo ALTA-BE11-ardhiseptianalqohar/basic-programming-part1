e6ffa395fabb6f1547825535b66364f85c0ad1e2
ardhiseptianalqohar <ardhiseptianalqohar@gmail.com>
2022-08-03 22:13:35 +0700

    basic-programming-part1

---
+++ problem3.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main()  {
+	//input: Kobar
+	var nama string = "Kobar"
+
+	//output: Hallo Kobar, Saya Golang bahasa yang sangat menyenangkan.
+	fmt.Println("Hallo", nama, "! Saya Golang bahasa yang sangat menyenangkan.")
+
+	
+}
