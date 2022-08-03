8a4462bc12b64921b7c8e5ed46c42c9fed84c1be
ardhiseptianalqohar <ardhiseptianalqohar@gmail.com>
2022-08-03 23:21:12 +0700

    basic-programming-part1

---
+++ problem4.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main()  {
+	//input: alas = 20, tinggi = 25
+var alas int
+alas = 20
+tinggi := 25
+
+	//output: 250
+	var luas = (alas * tinggi) / 2
+	fmt.Println(luas)
+
+}
